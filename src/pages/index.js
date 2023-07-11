311cf4e7894f48aa42130d6e72394cc1ef99fcb9
Ahmad Haydar Alkaf <78914402+ahmadhaydar@users.noreply.github.com>
2023-07-12 04:03:22 +0700

    Update index.js to use dvh dvw

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
   const [pokemonData, setPokemonData] = React.useState(null);
 
   return (
-    <Flex w="100vw" h="100vh">
+    <Flex w="100dvw" h="100dvh">
       <Flex
         p="5"
         flexDir="column"
