3a605647032e9e52afef6e930fda39bbef51ce3e
Haydar <alkaf.haydar@gmail.com>
2023-07-12 02:20:53 +0700

    feat: speedrunning pokemon list

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import 'src/styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
