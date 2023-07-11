12d893baccc6e852ccbf65ba6d64f5f37f1a861f
Haydar <alkaf.haydar@gmail.com>
2023-07-12 02:32:03 +0700

    feat: const

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
     }
   );
 
-  handleScroll = (e) => {
+  const handleScroll = (e) => {
     const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
     if (bottom && !isFetchingNextPage) fetchNextPage();
   }
